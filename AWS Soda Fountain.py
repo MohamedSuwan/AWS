f66e3baf436ee8a8445700fdaac7e4ea60dc916e
Mohamed Suwan <67264950+MohamedSuwan@users.noreply.github.com>
2022-09-15 11:51:31 +0300

    Add files via upload

---
+++ AWS Soda Fountain.py
@@ -0,0 +1,14 @@
+
+#  list of 4 soft drinks
+drinks=["Fanta","Pepsi","Sprite","CocaCola"]
+
+# list of sizes
+size=["small","medium","large","extra large"]
+
+print("Press here for a large Coke")
+
+# capitalize the list items
+capitalized=[x.upper() for x in drinks]
+
+# sort the list
+capitalized.sort()
