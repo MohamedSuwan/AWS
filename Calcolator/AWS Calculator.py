c3369d4fbd5ec298b224ba17967790cdcc331407
Mohamed Suwan <67264950+MohamedSuwan@users.noreply.github.com>
2022-09-18 11:23:49 +0300

    Create AWS Calculator.py

---
+++ Calcolator/AWS Calculator.py
@@ -0,0 +1,31 @@
+
+print("---------------------------------")
+operations=["+","-","*","/"]
+
+for i in range(4):
+    print(f"for {operations[i]} enter {i}")
+print("---------------------------------")
+
+operation=int(input("enter the number assosiated with the operation"))
+print("---------------------------------")
+
+print(f"you chose {operations[operation]}")
+print("---------------------------------")
+
+first=int(input("enter the first number"))
+second=int(input("enter the second number"))
+print("---------------------------------")
+
+def f():
+    if operation == 0:
+        print(first+second)
+    if operation == 1:
+        print(first+second)
+    if operation == 2:
+        print(first+second)
+    if operation == 3:
+        print(first+second)
+    else:
+        print("you entered a wrong number for the operation")
+
+f()
